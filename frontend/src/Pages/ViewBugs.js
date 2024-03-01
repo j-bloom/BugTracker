dc2fe6efcdb4c0e58932fa0c3ce93f3ba54fee36
Jaron Bloom <j-bloom7@protonmail.com>
2024-02-29 22:06:02 -0500

    fix spelling

--- frontend/src/Pages/ViewBugs.js
+++ frontend/src/Pages/ViewBugs.js
@@ -1,22 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { variables } from "../API/config.js";
 import axios from "axios";
 import Table from "../Components/Table.js";
-
-const headers = [
-  "Title",
-  "Description",
-  "Affected Area",
-  "Assigned to",
-  "Status",
-  "Priority",
-  "Date Created",
-  "Last Modified",
-];
 
 const ViewBugs = () => {
   const [Bug, setBug] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const headers = [
+    "Title",
+    "Description",
+    "Affected Area",
+    "Assigned to",
+    "Status",
+    "Priority",
+    "Date Created",
+    "Last Modified",
+  ];
 
   useEffect(() => {
     setLoading(true);
@@ -28,7 +27,7 @@ const ViewBugs = () => {
 
   return (
     <div>
-      <h1>Edit Bug</h1>
+      <h1>View Bugs</h1>
       <Table headers={headers} items={Bug} />
     </div>
   );
