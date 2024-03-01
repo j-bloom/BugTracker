6b9c2a450c96431ef34921f62f01ae296128d2c9
Jaron Bloom <j-bloom7@protonmail.com>
2024-02-29 22:04:21 -0500

    fix table to allow more flexibility when displaying bug info

--- frontend/src/Components/Table.js
+++ frontend/src/Components/Table.js
@@ -4,15 +4,55 @@ const Table = ({ headers, items }) => {
   function formattedDate(date) {
     const options = {
       year: "numeric",
-      month: "long",
+      month: "numeric",
       day: "numeric",
     };
     return new Date(date).toLocaleDateString("en-US", options);
   }
 
+  const StatusName = (status) => {
+    switch (status) {
+      case 0:
+        return "Open";
+      case 1:
+        return "In Progress";
+      case 2:
+        return "Closed";
+      default:
+        return "Unknown";
+    }
+  };
+
+  const renderBugList = (items) => {
+    if (headers.length > 5) {
+      return items.map((item, index) => (
+        <tr key={index}>
+          <td>{item.Title}</td>
+          <td>{item.Description}</td>
+          <td>{item.AffectedArea}</td>
+          <td>{item.Assignee}</td>
+          <td>{StatusName(item.Status)}</td>
+          <td>{item.Priority}</td>
+          <td>{formattedDate(item.DateBugCreated)}</td>
+          <td>{formattedDate(item.DateLastUpdated)}</td>
+        </tr>
+      ));
+    } else if (headers.length == 5) {
+      return items.map((item, index) => (
+        <tr key={index}>
+          <td>{item.Title}</td>
+          <td>{item.Description}</td>
+          <td>{item.Assignee}</td>
+          <td>{StatusName(item.Status)}</td>
+          <td>{formattedDate(item.DateBugCreated)}</td>
+        </tr>
+      ));
+    }
+  };
+
   return (
     <div>
-      <table className="table">
+      <table className="table table-striped">
         <thead>
           <tr>
             {headers.map((header, index) => (
@@ -20,21 +60,7 @@ const Table = ({ headers, items }) => {
             ))}
           </tr>
         </thead>
-        <tbody>
-          {/* TODO: GET data from DB and loop through to fill all columns */}
-          {items.map((item) => (
-            <tr>
-              <td>{item.Title}</td>
-              <td>{item.Description}</td>
-              <td>{item.AffectedArea}</td>
-              <td>{item.Assignee}</td>
-              <td>{item.Status}</td>
-              <td>{item.Priority}</td>
-              <td>{formattedDate(item.DateBugCreated)}</td>
-              <td>{formattedDate(item.DateLastUpdated)}</td>
-            </tr>
-          ))}
-        </tbody>
+        <tbody>{renderBugList(items)}</tbody>
       </table>
     </div>
   );
