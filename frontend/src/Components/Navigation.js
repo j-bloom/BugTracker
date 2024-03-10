e19b627be058b1ea2c9e82788d27f5029ae52f76
Jaron Bloom <j-bloom7@protonmail.com>
2024-03-10 01:26:18 -0500

    Update navbar to be more responsive

--- frontend/src/Components/Navigation.js
+++ frontend/src/Components/Navigation.js
@@ -3,24 +3,45 @@ import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-light navbar-dark">
-      <ul className="navbar-nav">
-        <li className="nav-item- m-1">
-          <Link className="btn btn-light btn-outline-primary" to="/home">
-            Home
-          </Link>
-        </li>
-        <li className="nav-item- m-1">
-          <Link className="btn btn-light btn-outline-primary" to="/createbug">
-            Create Bug
-          </Link>
-        </li>
-        <li className="nav-item- m-1">
-          <Link className="btn btn-light btn-outline-primary" to="/viewbugs">
-            View All Bugs
-          </Link>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item- m-1">
+              <Link className="btn btn-light btn-outline-primary" to="/home">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item- m-1">
+              <Link
+                className="btn btn-light btn-outline-primary"
+                to="/createbug"
+              >
+                Create Bug
+              </Link>
+            </li>
+            <li className="nav-item- m-1">
+              <Link
+                className="btn btn-light btn-outline-primary"
+                to="/viewbugs"
+              >
+                View All Bugs
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
